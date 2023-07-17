ed066772db11bc3066f54ff7df634b9ab13e15c5
Adam <amoore@uat.edu>
2023-07-17 16:21:08 -0700

    Added audio mixer and connected options menu sliders to mixer.

--- Assets/Scripts/Core/UIManager.cs
+++ Assets/Scripts/Core/UIManager.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    private const string MASTERVOLUME = "MasterVolumeParameter";
+    private const string SFXVOLUME = "SFXVolumeParameter";
+    private const string BGMVOLUME = "BGMVolumeParameter";
     public GameObject TitleScreenObject;
     public GameObject OptionsMenuObject;
     public GameObject PauseMenuObject;
     public GameObject GameOverObject;
+
+    public AudioMixer audioMixer;
+    public Slider mainVolumeSlider;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +27,52 @@ public class UIManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnMasterVolumeChange(float value)
+    {
+        // Start with the slider value (assuming our slider runs from 0 to 1)
+        float newVolume = ConvertToDecibel(value);
+
+        // Set the volume to the new volume setting
+        audioMixer.SetFloat(MASTERVOLUME, newVolume);
+    }
+
+    public void OnSFXVolumeChange(float value)
+    {
+        // Start with the slider value (assuming our slider runs from 0 to 1)
+        float newVolume = ConvertToDecibel(value);
+
+        // Set the volume to the new volume setting
+        audioMixer.SetFloat(SFXVOLUME, newVolume);
+    }
+
+    public void OnBGMVolumeChange(float value)
+    {
+        // Start with the slider value (assuming our slider runs from 0 to 1)
+        float newVolume = ConvertToDecibel(value);
+
+        // Set the volume to the new volume setting
+        audioMixer.SetFloat(BGMVOLUME, newVolume);
+    }
+
+    private float ConvertToDecibel(float value)
+    {
+        float newVolume = value;
+        if (newVolume <= 0)
+        {
+            // If we are at zero, set our volume to the lowest value
+            newVolume = -80;
+        }
+        else
+        {
+            // We are >0, so start by finding the log10 value 
+            newVolume = Mathf.Log10(newVolume);
+            // Make it in the 0-20db range (instead of 0-1 db)
+            newVolume = newVolume * 20;
+        }
+
+        return newVolume;
     }
 
     public void HideTitleScreenUI()
