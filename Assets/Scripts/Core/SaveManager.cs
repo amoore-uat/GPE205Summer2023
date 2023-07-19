d81523edfb05e738e2d93edfc59c7122e6bd07d8
Adam <amoore@uat.edu>
2023-07-19 16:26:18 -0700

    Refactor an AudioManager. Add a SaveManager

---
+++ Assets/Scripts/Core/SaveManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadPlayerPreferences();
+    }
+
+    public void SavePlayerPreferences()
+    {
+        PlayerPrefs.SetFloat("MasterVolume", AudioManager.Instance.masterVolume);
+        PlayerPrefs.SetFloat("BGMVolume", AudioManager.Instance.bgmVolume);
+        PlayerPrefs.SetFloat("SFXVolume", AudioManager.Instance.sfxVolume);
+    }
+
+    public void LoadPlayerPreferences()
+    {
+        if (PlayerPrefs.HasKey("MasterVolume"))
+        {
+            AudioManager.Instance.masterVolume = PlayerPrefs.GetFloat("MasterVolume");
+            AudioManager.Instance.OnMasterVolumeChange(AudioManager.Instance.masterVolume);
+        }
+        if (PlayerPrefs.HasKey("BGMVolume"))
+        {
+            AudioManager.Instance.bgmVolume = PlayerPrefs.GetFloat("BGMVolume");
+            AudioManager.Instance.OnBGMVolumeChange(AudioManager.Instance.bgmVolume);
+        }
+        if (PlayerPrefs.HasKey("SFXVolume"))
+        {
+            AudioManager.Instance.sfxVolume = PlayerPrefs.GetFloat("SFXVolume");
+            AudioManager.Instance.OnSFXVolumeChange(AudioManager.Instance.sfxVolume);
+        }
+
+    }
+
+
+}
